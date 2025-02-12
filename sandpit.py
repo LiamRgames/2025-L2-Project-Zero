3baef209fd0d07d1371d3a5288b0836e13594e3f
LiamRgames <124933116+LiamRgames@users.noreply.github.com>
2025-02-13 10:04:59 +1300

    Flexible Multi Line Decorator Component

---
+++ sandpit.py
@@ -0,0 +1,16 @@
+#Multiplication
+number = 10
+print(f"Multiplication: {number*3}")
+
+#Exponential
+number = 5
+print(f"Exponential: {number**3}")
+
+#String Duplication
+num_string = "5"
+print(f"String Duplication: {num_string*5}")
+
+#String Length
+text = "Hello my name is Groot"
+text_length = len(text)
+print(f"{text} is {text_length} characters long")
