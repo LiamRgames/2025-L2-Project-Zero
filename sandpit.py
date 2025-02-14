1bd31faeac6f08a1dab3d0f7a22247dee5d4651d
LiamRgames <124933116+LiamRgames@users.noreply.github.com>
2025-02-14 17:38:50 +1300

    String Checker Component

--- sandpit.py
+++ sandpit.py
@@ -14,3 +14,12 @@ print(f"String Duplication: {num_string*5}")
 text = "Hello my name is Groot"
 text_length = len(text)
 print(f"{text} is {text_length} characters long")
+
+
+#Extracting Strings And Letters
+languages = ["python", "mysql", "php", "html", "css", "javascript", "assembly"]
+
+for i in languages:
+    print(f"Is {i} the best programming language? ")
+    print(f"It starts with the letter {i[0]}")
+    print(f"The first 2 letters are {i[:2]}")
