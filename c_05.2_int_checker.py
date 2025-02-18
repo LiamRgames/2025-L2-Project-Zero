873e9fd833a64e551c42d576ffae4791626afd2d
LiamRgames <124933116+LiamRgames@users.noreply.github.com>
2025-02-19 10:23:08 +1300

    Integer Range Checker with Exit Code

--- c_05.2_int_checker.py
+++ c_05.2_int_checker.py
@@ -2,24 +2,25 @@ response = ''
 LOW = 5
 HIGH = 100
 loop = True
+skip_main = False
 def int_checker(question, int_type, exit_code):
     global loop
     global response
-    if int_type == "integer":
-        convert = int
-        error = "Please enter an integer greater than 0"
-    else:
-        error = "Please enter an integer greater than 0"
-        convert = float
-
-
+    global skip_main
 
     while True:
         response = input(question).lower()
         if response == exit_code:
             loop = False
             print("Exiting...")
+            skip_main = True
             return ''
+        if int_type == "integer":
+            convert = int
+            error = "Please enter an integer greater than 0"
+        else:
+            error = "Please enter an integer greater than 0"
+            convert = float
 
 
 
@@ -36,16 +37,23 @@ def int_checker(question, int_type, exit_code):
 
 while loop == True:
     test_int = int_checker("Enter a number greater than 0:\n", "integer", "xxx")
-    if LOW > test_int:
-        print(f"{test_int} is too low")
-    elif HIGH < test_int:
-        print(f"{test_int} is too high")
+    if skip_main == False:
+        if LOW > test_int:
+            print(f"{test_int} is too low")
+        elif HIGH < test_int:
+            print(f"{test_int} is too high")
+        else:
+            print(f"{test_int} is a valid integer")
     else:
-        print(f"{test_int} is a valid integer")
+        loop = False
+
     test_float = int_checker("Enter a float greater than 0:\n", "float", "xxx")
-    if LOW > test_float:
-        print(f"{test_float} is too low")
-    elif HIGH < test_float:
-        print(f"{test_float} is too high")
+    if skip_main == False:
+        if LOW > test_float:
+            print(f"{test_float} is too low")
+        elif HIGH < test_float:
+            print(f"{test_float} is too high")
+        else:
+            print(f"{test_float} is a valid float")
     else:
-        print(f"{test_float} is a valid float")
+        loop = False
